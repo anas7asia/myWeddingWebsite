d34383e47529cdcc6a4542c0b2bb5f6c50bac2b0
nikokosh <annikokosheva@yandex.ru>
2016-04-12 22:13:46 +0200

    Form added, new paddings, completed migration to Bootstrap

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,17 @@
 $(document).ready(function($){
     var nav = $('nav');
     var navLink = $('nav ul li a');
+    var navBrand = $('a.navbar-brand');
 
     $(window).scroll(function () {
-        if ($(this).scrollTop() > 15) {
+        if ($(this).scrollTop() > 10) {
             nav.addClass("nav_colored");
             navLink.addClass("nav-link_colored");
+            navBrand.addClass("nav-link_colored")
         } else {
             nav.removeClass("nav_colored");
             navLink.removeClass("nav-link_colored");
+            navBrand.removeClass("nav-link_colored");
         }
     });
 });
